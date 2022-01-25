2c6ed9fc9624f6f002433053c89f95cb5baf0ce2
Sanduler <50250694+sanduler@users.noreply.github.com>
2022-01-25 08:59:14 -0800

    added the data file and the cover art

---
+++ main.py
@@ -0,0 +1,6 @@
+# Name: Ruben Sanduleac
+# Date: January 25th, 2022
+# Description: This game the user puts themselfs to the test to see if they know
+#              which are the most popular influencers on Instagram.  Simply decide which Person
+#              has the most follower count searched for the most by selecting higher or lower.
+#              The objective is to get the most right in a row.
