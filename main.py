719ac7213028cc9628ebc8f2e89af6f00fb7ae59
Sanduler <50250694+sanduler@users.noreply.github.com>
2022-01-25 09:20:40 -0800

    finished the todo and improted the libraries

--- main.py
+++ main.py
@@ -4,3 +4,16 @@
 #              which are the most popular influencers on Instagram.  Simply decide which Person
 #              has the most follower count searched for the most by selecting higher or lower.
 #              The objective is to get the most right in a row.
+import random
+import data
+import cover_art
+
+# TODO Add higher lower logo
+# TODO randomly pickout from dictionary make, sure its not the same
+# TODO print the account name, discription, then country
+# TODO print the 'Vs' Logo
+# TODO print the vs account name, discription, then country
+# TODO print who has more follower_count
+# TODO compare and give user the prompt "Who has more followers? Type 'A' or 'B':"
+# TODO repeat propt until the user guesses incorrectly 'Sorry, that's wrong. Final score:'
+# TODO create a counter for final score
